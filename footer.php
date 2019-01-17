5e88fe5afdd8df59f3a1fbdb73b65bcde525abb3
Denis Nichik <deninichi@gmail.com>
2019-01-17 13:51:20 +0100

    New footer

--- footer.php
+++ footer.php
@@ -3,7 +3,19 @@
 		<footer>
 			<div class="container">
 				<div class="row">
-					<div class="col-12 col-lg-9">
+					<div class="col-12 text-center">
+						<div class="flags mb-5 mt-5">
+							<ul>
+								<li><img src="<?php echo get_template_directory_uri() . '/assets/images/flags/swe.png'; ?>" alt=""></li>
+								<li><img src="<?php echo get_template_directory_uri() . '/assets/images/flags/spain.png'; ?>" alt=""></li>
+								<li><img src="<?php echo get_template_directory_uri() . '/assets/images/flags/it.png'; ?>" alt=""></li>
+								<li><img src="<?php echo get_template_directory_uri() . '/assets/images/flags/arg.png'; ?>" alt=""></li>
+								<li><img src="<?php echo get_template_directory_uri() . '/assets/images/flags/col.png'; ?>" alt=""></li>
+								<li><img src="<?php echo get_template_directory_uri() . '/assets/images/flags/flag.png'; ?>" alt=""></li>
+								<li><img src="<?php echo get_template_directory_uri() . '/assets/images/flags/fra.png'; ?>" alt=""></li>
+								<li><img src="<?php echo get_template_directory_uri() . '/assets/images/flags/ger.png'; ?>" alt=""></li>
+							</ul>
+						</div>
 						<div class="footer-menu">
 							<?php wp_nav_menu( array(
 								'theme_location'  => 'footer_menu',
@@ -12,16 +24,14 @@
 							) ); ?>
 						</div>
 
-						<div class="footer-copy">
+						<div class="footer-copy mb-5">
 							<?php the_field( 'loans_footer_text', 'option' ); ?>
 						</div>
-					</div>
-					<div class="col-12 col-lg-3 mt-4 mt-lg-0 d-flex flex-column justify-content-between align-items-center">
-						<div class="dmca-logo">
-							<img src="<?php echo get_template_directory_uri() . '/assets/images/dmca.png'; ?>" alt="">
-						</div>
-						<div class="copyright">
-							<?php the_field( 'loans_footer_copyright', 'option' ); ?>
+						<div class="copyright mb-3">
+							<span class="logo">
+								<img src="<?php echo get_template_directory_uri() . '/assets/images/dmca-logo.png'; ?>" alt="">
+							</span>
+							<span><?php the_field( 'loans_footer_copyright', 'option' ); ?></span>
 						</div>
 					</div>
 				</div>
